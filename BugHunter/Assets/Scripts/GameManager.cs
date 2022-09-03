7c8640e0b0cec49d3378db98c3f4de07b0a2e018
Oroppo2 <72948908+Oroppo@users.noreply.github.com>
2022-09-03 01:40:22 -0400

    Loading Screen and ADS implemented

--- BugHunter/Assets/Scripts/GameManager.cs
+++ BugHunter/Assets/Scripts/GameManager.cs
@@ -6,29 +6,63 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    //TODO: will need to refactor this at some point.
+    [Header("Loading Screen")]
     public GameObject loadingScreen;
     public Image loadingBarFill;
 
+    [Header("Time Slow")]
+    public float timeSlowStrength = 0.05f;
+    public float timeSlowDuration = 1f;
+
     public void BulletTime()
     {
-         
+        Time.timeScale = timeSlowStrength;
+        Time.fixedDeltaTime = Time.timeScale * 0.02f;
+    }
+
+    public void Update()
+    {
+        Time.timeScale += (1f / timeSlowStrength) * Time.deltaTime;
+        Time.timeScale = Mathf.Clamp01(Time.timeScale);
     }
 
     public void SceneChange(string sceneName)
-    {
-        //Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
-       
+    {       
+        StartCoroutine(SceneChangeAsync(sceneName));
     }
 
     public void SceneChange(int sceneID)
     {
-        //Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
-        SceneManager.LoadScene(sceneID, LoadSceneMode.Additive);
+        StartCoroutine(SceneChangeAsync(sceneID));
     }
-    public void SceneChangeAsync(int sceneID)
+    public IEnumerator SceneChangeAsync(int sceneID)
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneID);
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(operation.progress / 0.9f);
+            loadingBarFill.fillAmount = progressValue;
+            yield return null;
+        }
     }
+    public IEnumerator SceneChangeAsync(string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(operation.progress / 0.9f);
+            loadingBarFill.fillAmount = progressValue;
+            yield return null;
+        }
+    }
+
 
     public void ScenePreLoad()
     {
