552d28cc5b91babe9ebb2161d13b8b1bd75b20d1
Purple7989 <kai.joseph1@ontariotechu.net>
2022-09-11 19:43:35 -0400

    Inventory System Part 3 (Deleting Items)
    You can now delete Items. Main changes are located in InventoryItemRemover.cs + InventoryManager.cs

--- BugHunter/Assets/Scripts/Inventory/InventoryManager.cs
+++ BugHunter/Assets/Scripts/Inventory/InventoryManager.cs
@@ -8,25 +8,29 @@ using UnityEngine.UI;
 // additionally assists in adding the items from the list into the player's UI inventory
 public class InventoryManager : MonoBehaviour
 {
+    // create an public static instance so other scripts can use this script's functions
     public static InventoryManager Instance;
     // List of our inventory items
     public List<Item> Items = new List<Item>();
+    public InventoryItemRemover[] InventoryItemArray;
 
     // where the items are filled in our UI inventory
     public Transform Content;
     // the 2D prefab version of the item
-    public GameObject InventoryItem2D;
+    public GameObject inventoryItem2D;
+
+    public Toggle enableItemRemoval;
 
     private void Awake()
     {
         Instance = this;
     }
-    // add an item to the iventory
+    // add an item to the iventory list
     public void Add(Item item)
     {
         Items.Add(item);
     }
-    // remove an item from the inventory
+    // remove an item from the inventory list
     public void Remove(Item item)
     {
         Items.Remove(item);
@@ -45,14 +49,49 @@ public class InventoryManager : MonoBehaviour
         // then change the UI inventory representation of them accordingly
         foreach(var item in Items)
         {
-            GameObject obj = Instantiate(InventoryItem2D, Content);
+            GameObject obj = Instantiate(inventoryItem2D, Content);
 
             var itemName = obj.transform.Find("ItemName_TMP").GetComponent<Text>();
             var itemIcon = obj.transform.Find("Image").GetComponent<Image>();
 
             itemName.text = item.itemName;
             itemIcon.sprite = item.icon;
-
+        }
+        // when our list of items updates so does our array
+        SetInventoryArray();
+    }
+    //This is called when the Delete Items toggle in the inventory menu is clicked
+    // when its clicked on it will unhide the remove button for each inventory item
+    // when it's clicked off it will hide the remove button for each inventory item
+    public void DeleteInventoryItems()
+    {
+        //Unhide removal buttons
+        if(enableItemRemoval.isOn)
+        {
+            foreach(Transform item in Content)
+            {
+                item.Find("RemoveButton").gameObject.SetActive(true);
+            }
+        }
+        //Hide removal buttons
+        else
+        {
+            foreach (Transform item in Content)
+            {
+                item.Find("RemoveButton").gameObject.SetActive(false);
+            }
         }
     }
+    
+    public void SetInventoryArray()
+    {
+        //set our array to be equal to all children in content who have the component item remover (the 2D prefab items)
+        InventoryItemArray = Content.GetComponentsInChildren<InventoryItemRemover>();
+        //Go through each item in the inventory List and add it to the array
+        for(int i = 0; i < Items.Count; i++)
+        {
+            InventoryItemArray[i].AddItem(Items[i]);
+        }
+    }
+
 }
--- BugHunter/Assets/Scripts/Inventory/InventoryOpener.cs
+++ BugHunter/Assets/Scripts/Inventory/InventoryOpener.cs
@@ -1,26 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 // this script is used to open the inventory using a button press
 // and call the inventory manager script when inventory is opened
 public class InventoryOpener : MonoBehaviour
 {
     public GameObject inventoryUI;
+    public Toggle RemovalToggle;
     public InventoryManager iManager;
 
     public void Update()
     {
         openInventory();
     }
-
+    // when the player opens inventory set the states of the inventory to their default state
     public void openInventory()
     {
-        if (Input.GetKeyDown(KeyCode.I))
+        if(inventoryUI.activeSelf == false)
         {
-            inventoryUI.SetActive(true);
-            Cursor.lockState = CursorLockMode.None;
-            iManager.ListItems();
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                inventoryUI.SetActive(true);
+                RemovalToggle.isOn = false;
+                Cursor.lockState = CursorLockMode.None;
+                iManager.ListItems();
+            }
         }
     }
 }
