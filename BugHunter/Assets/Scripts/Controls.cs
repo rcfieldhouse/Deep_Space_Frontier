c7a4937f7aa01e5d0e114085075c93a026278efe
Oroppo2 <72948908+Oroppo@users.noreply.github.com>
2022-09-18 19:58:17 -0400

    player controls

--- BugHunter/Assets/Scripts/Controls.cs
+++ BugHunter/Assets/Scripts/Controls.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class NetworkManager : MonoBehaviour
+using UnityEngine.InputSystem;
+public class Controls : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
