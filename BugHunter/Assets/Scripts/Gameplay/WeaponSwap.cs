4f7fdc9c7c8718f74cebd6592fb63c6b4c4e445c
Alexi <alexitrae@gmail.com>
2022-09-30 13:40:32 -0400

    RyanChanges

--- BugHunter/Assets/Scripts/Gameplay/WeaponSwap.cs
+++ BugHunter/Assets/Scripts/Gameplay/WeaponSwap.cs
@@ -29,18 +29,37 @@ public class WeaponSwap : MonoBehaviour
     }
     public void SetWeapon(int num)
     {
-        if (WeaponArray[num] != null)
-        {
+       // if (WeaponArray[num])
+       // {
             for (int i = 0; i < WeaponArray.Length; i++)
             {
                 RecticleArray[i].SetActive(false);
                 WeaponArray[i].SetActive(false);
             }
+        Debug.Log(WeaponArray.Length);
 
+        if (num < 0)
+        {
+            RecticleArray[0].SetActive(true);
+            WeaponArray[0].SetActive(true);
+            WeaponChoice = 0;
+        }
+            else if (num > WeaponArray.Length-1)
+        {
+          
+            RecticleArray[WeaponArray.Length-1].SetActive(true);
+            WeaponArray[WeaponArray.Length-1].SetActive(true);
+            WeaponChoice = WeaponArray.Length-1;
+
+        }
+            else if (num >= 0 && num <= WeaponArray.Length)
+        {
             RecticleArray[num].SetActive(true);
             WeaponArray[num].SetActive(true);
             WeaponChoice = num;
         }
+         
+        //}
        
     }
 }
