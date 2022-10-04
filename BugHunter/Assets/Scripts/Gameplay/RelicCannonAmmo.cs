8b2ca8473710286a155c1ce3e32a066bc349fd14
rcfieldhouse <rcfieldhouse@yahoo.com>
2022-10-04 12:24:35 -0400

    de gun anihilates all things in existance

--- BugHunter/Assets/Scripts/Gameplay/RelicCannonAmmo.cs
+++ BugHunter/Assets/Scripts/Gameplay/RelicCannonAmmo.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class RelicCannonAmmo : MonoBehaviour
 {
+    private WaitForSeconds BoomTimer = new WaitForSeconds(0.5f);
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +14,19 @@ public class RelicCannonAmmo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        DestroyThis();
+       
     }
-    private void DestroyThis()
+    private void OnCollisionEnter(Collision collision)
     {
-        Destroy(this);
+        if (collision.gameObject.tag=="Enemy")
+        collision.gameObject.GetComponentInParent<HealthSystem>().ModifyHealth(-100); 
+
+        StartCoroutine(DestroyThis());
+    }
+    private IEnumerator DestroyThis()
+    {
+        gameObject.transform.localScale = new Vector3(10.0f, 6.0f, 10.0f);
+        yield return BoomTimer;
+        Destroy(gameObject);
     }
 }
