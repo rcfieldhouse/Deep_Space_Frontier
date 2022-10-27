7fba3d1b015f324f713d1652758a996b27abaa33
rcfieldhouse <rcfieldhouse@yahoo.com>
2022-10-27 19:21:57 -0400

    start of class creation

---
+++ BugHunter/Assets/Scripts/Factories/ClassCreator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassCreator : MonoBehaviour
+{
+    public ClassType foo;
+    public enum ClassType
+    {
+        Assault, Engineer, Sniper
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+   
+}
