46e2a24d42d616705ff720b4e3db92cae9dfd201
rcfieldhouse <rcfieldhouse@yahoo.com>
2022-11-14 15:20:23 -0500

    basic spawner functionality

--- BugHunter/Assets/Scripts/Spawner.cs
+++ BugHunter/Assets/Scripts/Spawner.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+public enum EnemyType
+{
+   Hound, DreadBomber, Golem, Worm, WalmartTorterra
+}
 public class Spawner : MonoBehaviour
 {
-    public GameObject prefab;
-    [SerializeField] private WaitForSeconds lungeWait = new WaitForSeconds(0.25f);
+    public EnemyType EnemySelection;
+    public List<GameObject> prefab;
+    [Range(0, 30)] public float SpawnTimer=0.0f;
+
+   [SerializeField] private WaitForSeconds lungeWait = new WaitForSeconds(0.25f);
     [SerializeField] private WaitForSeconds lungeDuration = new WaitForSeconds(1.5f);
     [SerializeField] private WaitForSeconds SwingDuration = new WaitForSeconds(0.75f);
-    [SerializeField] private WaitForSeconds SpawnTimer = new WaitForSeconds(0.1f);
+
+ 
+     private WaitForSeconds SpawnTime = new WaitForSeconds(1.0f);
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(Iterator());
+        SpawnTime = new WaitForSeconds(SpawnTimer);
+        SelectEnemy(EnemySelection);
     }
 
     // Update is called once per frame
@@ -20,16 +29,47 @@ public class Spawner : MonoBehaviour
     {
        
     }
-
-    private IEnumerator Iterator()
+    public void SelectEnemy(EnemyType TypeEnemy)
+    {
+        switch (TypeEnemy)
+        {
+            case EnemyType.Hound:
+                StartCoroutine(HoundSpawn());
+                break;
+            case EnemyType.DreadBomber:
+                StartCoroutine(DreadBomberSpawn());
+                break;
+            case EnemyType.Golem:
+                StartCoroutine(HoundSpawn());
+                break;
+            case EnemyType.Worm:
+                StartCoroutine(HoundSpawn());
+                break;
+            case EnemyType.WalmartTorterra:
+                StartCoroutine(HoundSpawn());
+                break;
+        }
+    }
+    private IEnumerator HoundSpawn()
     {
         //this is the not flyweight way
         // yield return new WaitForSeconds(5.0f);
-        Debug.Log("meow");
+     
         //this is the flyweight way
-        yield return SpawnTimer;
-       Instantiate(prefab, this.gameObject.transform).GetComponent<GroundAi>().SetTimes(lungeWait, lungeDuration, SwingDuration);
+        yield return SpawnTime;
+       Instantiate(prefab[0], this.gameObject.transform).GetComponent<GroundAi>().SetTimes(lungeWait, lungeDuration, SwingDuration);
 
-        StartCoroutine(Iterator());
+        StartCoroutine(HoundSpawn());
+    }
+    private IEnumerator DreadBomberSpawn()
+    {
+        //this is the not flyweight way
+        // yield return new WaitForSeconds(5.0f);
+
+        //this is the flyweight way
+        yield return SpawnTime;
+        Instantiate(prefab[1], this.gameObject.transform);
+
+        StartCoroutine(DreadBomberSpawn());
     }
 }
