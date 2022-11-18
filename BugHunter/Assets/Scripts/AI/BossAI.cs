ae1940ab15befd521a6062d4d737e3fc1a42c793
rcfieldhouse <rcfieldhouse@yahoo.com>
2022-11-18 13:00:21 -0500

    start of boss AI

--- BugHunter/Assets/Scripts/AI/BossAI.cs
+++ BugHunter/Assets/Scripts/AI/BossAI.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class BossAI : MonoBehaviour
 {
+    [SerializeField] private LayerMask whatIsGround, whatIsPlayer;
+    public float FrontAttackRange, BackAttackRange, SeekRange;
+    public Vector3 FrontAttackPos, BackAttackPos;
+   public bool PlayerInFrontAttackRange, PlayerInBackAttackRange, PlayerInSeekRange;
     public Transform target;
     // Start is called before the first frame update
     void Start()
@@ -14,6 +18,18 @@ public class BossAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(target);
+        PlayerInFrontAttackRange = Physics.CheckSphere(transform.position + transform.rotation * FrontAttackPos, FrontAttackRange, whatIsPlayer);
+        PlayerInBackAttackRange = Physics.CheckSphere(transform.position + transform.rotation * BackAttackPos, BackAttackRange, whatIsPlayer);
+        PlayerInSeekRange = Physics.CheckSphere(transform.position , SeekRange, whatIsPlayer);
+        //transform.LookAt(target);
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position+transform.rotation* FrontAttackPos, FrontAttackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position + transform.rotation * BackAttackPos, BackAttackRange);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position ,SeekRange);
     }
 }
