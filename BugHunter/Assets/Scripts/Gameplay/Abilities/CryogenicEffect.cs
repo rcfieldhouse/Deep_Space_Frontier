0581474ba47048fecdba690777227d957498813c
Dante Arruda <72948908+Oroppo@users.noreply.github.com>
2022-11-20 18:47:30 -0500

    Ice Material
    boo ba

--- BugHunter/Assets/Scripts/Gameplay/Abilities/CryogenicEffect.cs
+++ BugHunter/Assets/Scripts/Gameplay/Abilities/CryogenicEffect.cs
@@ -5,27 +5,33 @@ using UnityEngine.AI;
 public class CryogenicEffect : MonoBehaviour
 {
     private WaitForSeconds FreezeTime = new WaitForSeconds(3.0f);
-   
+    private Material BaseMat;
+    private Vector4 FrozenColor = new Vector4(0f,0.5f,1f,1f);
+    private Material FrozenMat = Resources.Load("Frozen", typeof(Material)) as Material;
+    
+
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
     private void Awake()
     {
+        BaseMat = GetComponentInChildren<MeshRenderer>().material;
+
         //called when attached to de enemy 
         StartCoroutine(FreezeDeBoi());
     }
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private IEnumerator FreezeDeBoi()
     {
-        gameObject.GetComponent<NavMeshAgent>().enabled=false;
+        GetComponentInChildren<MeshRenderer>().material = FrozenMat;
+
+
+        GetComponent<NavMeshAgent>().enabled=false;
         yield return FreezeTime;
-        gameObject.GetComponent<NavMeshAgent>().enabled =true;
+        GetComponentInChildren<MeshRenderer>().material = BaseMat;
+
+        //BaseMat.SetVector("DamageColor", new Vector4(1, 1, 1, 1));
+
+        GetComponent<NavMeshAgent>().enabled =true;
+
         Destroy(this);
     }
 }
