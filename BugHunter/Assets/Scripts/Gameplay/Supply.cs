4c5ed9133cb1000e491c3895ec189e1607b23cd9
rcfieldhouse <rcfieldhouse@yahoo.com>
2022-11-21 18:51:09 -0500

    supply drop

---
+++ BugHunter/Assets/Scripts/Gameplay/Supply.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Supply : MonoBehaviour
+{
+    bool Deployed = false;
+    public List<GameObject> SpawnPoints; 
+    public List<GameObject> LootDrops; 
+    // Start is called before the first frame update
+    private void Start()
+    {
+      
+        for (int i = 0; i<transform.childCount; i++)
+        {
+            SpawnPoints.Add(transform.GetChild(i).gameObject);
+        }
+        GetComponent<Rigidbody>().isKinematic = true;
+        Drop();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (Deployed == false)
+        {
+            DeployLoot();
+        }
+        
+        Deployed = true;
+    }
+    // Update is called once per frame
+  
+    void DeployLoot()
+    {
+        if (Deployed == false)
+        {
+
+      
+            for (int i = 0; i < LootDrops.Count; i++)
+             {
+            for (int j=0; j < SpawnPoints.Count; j++)
+            {
+                Debug.Log("Spawned loot " + j + " at " + LootDrops);
+                Instantiate(LootDrops[i], SpawnPoints[j].transform);
+            }
+             }
+        }
+    }
+   public void Drop()
+    {
+        GetComponent<Rigidbody>().isKinematic = false;
+    }
+}
