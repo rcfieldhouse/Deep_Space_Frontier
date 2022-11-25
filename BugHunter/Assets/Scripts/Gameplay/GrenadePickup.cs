b9b6977a9d08cbf89d149c47c39f647f6958330e
rcfieldhouse <rcfieldhouse@yahoo.com>
2022-11-24 20:32:52 -0500

    u can specify how many nades u want

--- BugHunter/Assets/Scripts/Gameplay/GrenadePickup.cs
+++ BugHunter/Assets/Scripts/Gameplay/GrenadePickup.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class GrenadePickup : MonoBehaviour
 {
+    [Range(0, 5)] public int numGrenades=1;
     // Start is called before the first frame update
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             //do the thing
-            collision.gameObject.GetComponent<GrenadeManager>().GainGrenades();
+            collision.gameObject.GetComponent<GrenadeManager>().GainGrenades(numGrenades);
 
             //shhh this is my little sneaky, the action is attached to the render thing 
             //basically it updates the UI
