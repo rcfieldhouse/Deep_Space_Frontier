357f5e0f5a209b533bb187f26bef2b83028399dd
rcfieldhouse <rcfieldhouse@yahoo.com>
2022-12-12 16:57:35 -0500

    improved material drop

--- BugHunter/Assets/Scripts/Inventory/MaterialDrop.cs
+++ BugHunter/Assets/Scripts/Inventory/MaterialDrop.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class MaterialDrop : MonoBehaviour
 {
-    public MaterialTypes Material; 
+    public List<MaterialTypes> MaterialAtThisIndex;
+    [Range(0, 10)] public List<int> NumMaterialsAtThisIndex;
+    public bool RandomDrop = false;
+
     // Start is called before the first frame update
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            LootSpawner.instance.DropMaterials(transform, (int)Material);
+            for (int i=0; i < NumMaterialsAtThisIndex.Count; i++)
+            {
+                for (int j=0;j< NumMaterialsAtThisIndex[i];j++)
+                    if(RandomDrop==false)
+                LootSpawner.instance.DropMaterials(transform, (int)MaterialAtThisIndex[i]);
+                else if (RandomDrop == true)
+                    {
+                      
+                        LootSpawner.instance.DropMaterials(transform, (int)Random.Range(0, 6));
+                    }
+                        
+            }
+           
             Destroy(gameObject);
         }
     }
