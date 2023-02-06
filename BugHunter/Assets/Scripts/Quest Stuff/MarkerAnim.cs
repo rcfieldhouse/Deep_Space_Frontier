1ad2fa52afb661e9654364d1642682918970d53a
rcfieldhouse <rcfieldhouse@yahoo.com>
2023-02-06 15:54:24 -0500

    start of quest indicator

---
+++ BugHunter/Assets/Scripts/Quest Stuff/MarkerAnim.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkerAnim : MonoBehaviour
+{
+    GameObject Parent;
+    int Direction = 1;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Parent = transform.parent.gameObject;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position += Direction*Vector3.up / 10;
+        if (Mathf.Abs(transform.position.y - Parent.transform.position.y) > 2.0f)
+        {
+            Direction *= -1;
+        }
+    }
+}
