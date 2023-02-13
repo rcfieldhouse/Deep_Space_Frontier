88c03821aa03e80dc79d8203e19072440584675d
rcfieldhouse <rcfieldhouse@yahoo.com>
2023-02-13 15:55:16 -0500

    boss fight

--- BugHunter/Assets/Scripts/AI/NEW AI/ForestDweller.cs
+++ BugHunter/Assets/Scripts/AI/NEW AI/ForestDweller.cs
@@ -7,7 +7,7 @@ public class ForestDweller : AI
     [Range(0,50)] public float LaunchHeight;
     [Range(0, 15)] public float TimeBetweenJumps;
    public Rigidbody Rigidbody;
-    public bool _CanJump = false;
+    private bool _CanJump = false,RoarPossible=false,HasFrontAttacked=false;
     public override void Update()
     {
         bool playerInSightRange = Physics.CheckSphere(transform.position + transform.rotation * SightRangeOffset, _SightRange, WhatIsPlayer);
@@ -34,16 +34,51 @@ public class ForestDweller : AI
     }
     public override void AttackPlayer(GameObject Target)
     {
-        Debug.Log("attacking");
+        if (CanAttack == true && HasAttacked == false)
+        {
+            if (HasFrontAttacked == true)
+                IsSecondaryAttack = true;
+            if (IsSecondaryAttack == true)
+            {
+                SecondaryAttack(Target);
+                return;
+            }
+               //play Dante.sound.ogg torterra attack
+                AI_Animator.SetBool("_IsAttacking", true);
+                Target.GetComponent<HealthSystem>().ModifyHealth(Attack_1_Damage);
+                HasAttacked = true;
+                CanAttack = false;
+            HasFrontAttacked = true;
+        }
+        if (HasAttacked == true)
+        {
+            HasAttacked = false;
+           Invoke(nameof(ResetAttack), Attack_1_Delay);
+            Invoke(nameof(StopAnim), 1.0f);
+        }
+    }
+    void SecondaryAttack(GameObject Target)
+    {
+        AI_Animator.SetBool("_IsAttacking2", true);
+        HasAttacked = true;
+        CanAttack = false;
+        HasFrontAttacked = false;
+        Target.GetComponent<HealthSystem>().ModifyHealth(Attack_2_Damage);
+        Invoke(nameof(ResetAttack), Attack_2_Delay);
+        Invoke(nameof(StopAnim), 1.0f);
     }
     public override void Patroling()
     {
+        RoarPossible = true;
         NavAgent.enabled = false;
        //do nothing bc this man is a boss and he doesn't need to patrol
        //he is the one who knocks
     }
     public void LaunchTheBoss()
     {
+        if (RoarPossible == true)
+            DoTheRoar();
+
         if (Target == null)
             Target = FindClosestPlayer();
 
@@ -60,8 +95,18 @@ public class ForestDweller : AI
             Invoke(nameof(StopAnim), 1.0f);
         }
     }
+    void DoTheRoar()
+    {
+        RoarPossible = false;   
+        AI_Animator.SetBool("_Roar", true);
+        Invoke(nameof(ResetJump),4.0f);
+        Invoke(nameof(StopAnim), 1.0f);
+    }
     public void StopAnim()
     {
+        AI_Animator.SetBool("_IsAttacking2", false);
+        AI_Animator.SetBool("_IsAttacking", false);
+        AI_Animator.SetBool("_Roar", false);
         AI_Animator.SetBool("_IsMoving", false);
     }
     public void ResetJump()
