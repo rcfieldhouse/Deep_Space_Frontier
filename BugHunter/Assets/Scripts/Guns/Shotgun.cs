97b4b36c6a0ebae48758157c36641ff8f8147949
rcfieldhouse <rcfieldhouse@yahoo.com>
2023-03-02 16:22:22 -0500

    new shotgun spread

--- BugHunter/Assets/Scripts/Guns/Shotgun.cs
+++ BugHunter/Assets/Scripts/Guns/Shotgun.cs
@@ -5,22 +5,44 @@ using UnityEngine;
 public class Shotgun : Gun
 {
     private Vector3 ShotgunSpread;
+    public static float RandomGaussian(float minValue = 0.0f, float maxValue = 1.0f)
+    {
+        float u, v, S;
+
+        do
+        {
+            u = 2.0f * UnityEngine.Random.value - 1.0f;
+            v = 2.0f * UnityEngine.Random.value - 1.0f;
+            S = u * u + v * v;
+        }
+
+        while(S >= 1.0f);
+
+        // Standard Normal Distribution
+        float std = u * Mathf.Sqrt(-2.0f * Mathf.Log(S) / S);
+
+        // Normal Distribution centered between the min and max value
+        // and clamped following the "three-sigma rule"
+        float mean = (minValue + maxValue) / 2.0f;
+        float sigma = (maxValue - mean) / 3.0f;
+        return Mathf.Clamp(std * sigma + mean, minValue, maxValue);
+    }
     public override void Shoot()
     {
         if (info.GetCanShoot() == false|| gameObject.activeInHierarchy == false)
             return;
-        for (int i = 2; i <= 15; i += 2)
+        for (int i = 2; i <= 23; i += 2)
         {
             LazerLine.SetPosition(i, GunEnd.position);
         }
         info.SetCanShoot(false);
         info.SetBulletCount();
         NextFire = Time.time + FireRate;
-
-        for (int i = 1; i <= 15; i += 2)
+       
+        for (int i = 1; i <= 23; i += 2)
         {
-            ShotgunSpread.x = Random.Range(-ShotSpread.x, ShotSpread.x);
-            ShotgunSpread.y = Random.Range(-ShotSpread.y, ShotSpread.y);
+            ShotgunSpread.x = RandomGaussian(-ShotSpread.x, ShotSpread.x);
+            ShotgunSpread.y = RandomGaussian(-ShotSpread.y, ShotSpread.y);
             ShotgunSpread.z = 0.0f;
 
             if (_IsAiming == true)
