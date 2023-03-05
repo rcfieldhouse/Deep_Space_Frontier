7d982ad1f6674ba6abed42fdfec00c7b444fa2a2
rcfieldhouse <rcfieldhouse@yahoo.com>
2023-03-04 19:28:12 -0500

    shop stuff for kai

--- BugHunter/Assets/Shop.cs
+++ BugHunter/Assets/Shop.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Shop : MonoBehaviour
 {
-    public GameObject ShopInterface;
-    
-    public GameObject Player;
-    public GUIHolder gui;
+   private GameObject ShopInterface;
+   private GameObject Player;
+   private GUIHolder gui;
     private PlayerInput PlayerInput;
+    public ShopType TypeOfShop;
     // Start is called before the first frame update
-
+    public enum ShopType
+    {
+        LevelSelect, EquipmentLocker, CraftingMenu
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
@@ -19,6 +22,12 @@ public class Shop : MonoBehaviour
             gui = Player.transform.parent.GetComponentInChildren<GUIHolder>();
             gui.PickupPrompt.SetActive(true);
             //ShopInterface = gui.CraftingMenu;
+            if(TypeOfShop==ShopType.CraftingMenu)
+                ShopInterface = gui.CraftingMenu;
+            if(TypeOfShop==ShopType.EquipmentLocker)
+                ShopInterface = gui.EquipmentLocker;
+            if (TypeOfShop == ShopType.LevelSelect)
+                ShopInterface = gui.LVLSelect;
             PlayerInput = Player.GetComponent<PlayerInput>();
             PlayerInput.Interact += ToggleShop;
         }
@@ -31,6 +40,7 @@ public class Shop : MonoBehaviour
             gui.PickupPrompt.SetActive(false);
             // added this to disable the shop canvas
             ShopInterface.SetActive(false);
+            ShopInterface = null;
             Cursor.lockState = CursorLockMode.Locked;
             Player = null;
             // commented this out since I couldn't re-open canvases in game because of it
@@ -45,18 +55,22 @@ public class Shop : MonoBehaviour
         if (Player == null)
             return;
         Debug.Log("Shop is Opened :(");
-
+        bool toggle = !ShopInterface.activeInHierarchy;
         // i only need the canvas to activate not everything attached to it for tab switching
         //ShopInterface.SetActiveRecursively(!ShopInterface.activeInHierarchy);
-        ShopInterface.SetActive(true);
+        ShopInterface.SetActive(toggle);
 
-        bool toggle = ShopInterface.activeInHierarchy;
+       
 
-        gui.PickupPrompt.SetActive(false);
+        gui.PickupPrompt.SetActive(!toggle);
+
+         Player.transform.parent.GetComponentInChildren<WeaponInfo>().SetPaused(toggle);
+         Player.transform.parent.GetComponentInChildren<WeaponInfo>().SetIsReloading(toggle);
+         Player.transform.parent.GetComponentInChildren<Look>().SetIsPaused(toggle);
         //this is only so that esc is able to close the vendor for the sake of user integrity
-        Player.transform.parent.GetComponentInChildren<WeaponInfo>().SetCanShoot(!toggle);
-        Player.transform.parent.GetComponentInChildren<WeaponInfo>().SetIsReloading(toggle);
-        Player.transform.parent.GetComponentInChildren<Look>().SetIsPaused(!toggle);
+        
+        
+        
 
         if (toggle == true) Cursor.lockState = CursorLockMode.None;
         else if (toggle == false) Cursor.lockState = CursorLockMode.Locked;
