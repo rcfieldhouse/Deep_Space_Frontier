68177c12316f74256d30020de7d36b7f221bd4f6
rcfieldhouse <rcfieldhouse@yahoo.com>
2023-03-13 11:12:24 -0400

    Update INetworkable.cs

--- BugHunter/Assets/Networking/INetworkable.cs
+++ BugHunter/Assets/Networking/INetworkable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface INetworkable
+public interface INetwork
 {
     struct KeyValuePair<TKey, Tvalue>
     {
